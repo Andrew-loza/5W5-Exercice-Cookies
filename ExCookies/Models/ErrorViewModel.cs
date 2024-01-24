569d82e744d630c840f6b14ccb95a64b74730319
Andrew <2250757@cegepmontpetit.ca>
2024-01-24 10:14:02 -0500

    INITÉ Début exercice

---
+++ ExCookies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExCookies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
